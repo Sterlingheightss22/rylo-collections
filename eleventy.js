41538a6167ab524dbd51e1aea175d19e90792db3
Sterling <sterlingheights065@gmail.com>
2026-05-16 18:41:33 +0000

    Add products collection to Eleventy config

--- eleventy.js
+++ eleventy.js
@@ -1,7 +1,11 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("images");
-  
+
+  eleventyConfig.addCollection("products", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("_products/*.md");
+  });
+
   return {
     dir: {
       input: ".",
